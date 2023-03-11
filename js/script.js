952ea0de2a6b174d91daf936c4019736e58db869
tindrasjoberg <119052314+tindrasjoberg@users.noreply.github.com>
2023-03-11 22:03:55 +0100

    mobil anpassar( inte menu)

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 // window.addEventListener ("scroll", funktion() {
 //     header.classList.toggle ("sticky", window.scrollY > 100);
 // });
+
+let menu = document.querySelector('#menu-icon');
+let navliist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
