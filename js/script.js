84de4e4cdaa03c694279ac55abd86f81640d0eca
tindrasjoberg <119052314+tindrasjoberg@users.noreply.github.com>
2023-04-02 16:22:53 +0200

    små saker

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
+    menu.classList.toggle('meny-open');
     navlist.classList.toggle('open');
 };
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
+    menu.classList.remove('meny-open');
     navlist.classList.remove('open');
 };
